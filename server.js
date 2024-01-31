421bce38de58199200efeb012b9f974e8fe856b4
Hussein Saad <hussein.saad2021@gmail.com>
2024-01-31 05:17:56 +0200

    Add MongoDB connection to server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 const app = require('./app');
+
+const mongoDB = process.env.MONGODB_URL;
+
+connectDB();
+
+async function connectDB() {
+  try {
+    await mongoose.connect(mongoDB);
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
 const PORT = process.env.PORT || 3000;
 
