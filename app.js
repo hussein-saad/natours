c77a292ce197b757abca3b6b49f7e2a96f9b9405
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-06 22:27:33 +0200

    Add error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const CustomError = require('./utils/customError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
@@ -15,5 +18,11 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new CustomError(`${req.originalUrl} NOT FOUND`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
