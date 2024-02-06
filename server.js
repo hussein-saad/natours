fc12356e0b338a309f3518ae7efe13dca3328d78
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-06 22:25:52 +0200

    Add uncaught exception handler to server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 require('dotenv').config();
 const app = require('./app');
 
