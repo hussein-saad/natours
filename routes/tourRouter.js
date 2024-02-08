fa8439418a5080b773efafae67cefaf26361f04f
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-08 16:03:36 +0200

    Add authentication middleware to tour routes

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -10,12 +11,16 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guid'),
+    tourController.deleteTour,
+  );
 
 module.exports = router;
