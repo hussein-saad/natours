e1abcb904734e46f59676a6c62133fa5eec3aebe
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-08 16:00:46 +0200

    Add rate limiter middleware to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimiter = require('express-rate-limiter');
 
 const CustomError = require('./utils/CustomError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,6 +13,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimiter({
+  max: 100,
+  windowMS: 15 * 60 * 1000,
+  message: 'too many requests, please try again in 15 minutes',
+});
+
+app.use(limiter);
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
