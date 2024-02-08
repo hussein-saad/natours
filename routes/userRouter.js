8e3961055913f6787df0c31da47958bcae461069
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-08 16:03:20 +0200

    Add authentication routes and controllers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,23 @@
 const express = require('express');
 
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgetPassword', authController.forgetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.patch('/deleteMe', authController.protect, userController.deleteMe);
+
+router.patch(
+  '/updatePassword',
+  authController.protect,
+  authController.updatePassword,
+);
 
 router
   .route('/')
