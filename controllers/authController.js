640851c6c1f9936abd788f11c2913dfb9621bd93
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-08 16:02:14 +0200

    Add authController.js with authentication and authorization functionality

---
+++ controllers/authController.js
@@ -0,0 +1,198 @@
+const util = require('util');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const CustomError = require('./../utils/CustomError');
+const sendEmail = require('./../utils/email');
+
+const sendToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = sendToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: { user },
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+    photo: req.body.photo,
+    role: req.body.role,
+  });
+
+  createSendToken(user, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email) {
+    return next(new CustomError('email not provided', 400));
+  }
+
+  if (!password) {
+    return next(new CustomError('password not provided', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.checkPassword(password, user.password))) {
+    return next(new CustomError('Incorrect email or password', 401));
+  }
+
+  createSendToken(user, 200, res);
+});
+
+exports.forgetPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new CustomError('there is no user with that email', 404));
+  }
+
+  const resetToken = user.createResetTokenPassword();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password?\nsubmit a request with your new password to: ${resetURL}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'password reset token',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'token has been sent to your email',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new CustomError('there was an error sending the email', 500));
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new CustomError('token is invalid or has been expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save();
+
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.checkPassword(req.body.currentPassword, user.password))) {
+    return next(new CustomError('wrong password, please try again', 401));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    next(new CustomError('please log in to see the resources', 401));
+  }
+
+  const decode = await util.promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET,
+  );
+
+  const user = await User.findById(decode.id);
+
+  if (!user) {
+    return next(new CustomError('the user no longer exist', 401));
+  }
+
+  if (user.isPasswordChanged(decode.iat)) {
+    return next(
+      new CustomError('user password has been changed, please login agin', 401),
+    );
+  }
+
+  req.user = user;
+
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new CustomError("you don't have the permission to do this action", 403),
+      );
+    }
+
+    next();
+  };
+};
