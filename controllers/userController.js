adc701e59580355961e50e8e74571592109e0f47
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-08 16:03:08 +0200

    Add updateMe and deleteMe functions, and update getAllUsers function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,51 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not defined',
-  });
+const User = require('./../models/userModel');
+const CustomError = require('./../utils/CustomError');
+const catchAsync = require('./../utils/catchAsync');
+
+const filterBody = (body, ...allowedFields) => {
+  let ret = {};
+  for (const key in body) {
+    if (allowedFields.includes(key)) {
+      ret[key] = body[key];
+    }
+  }
+  return ret;
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new CustomError("you aren't allowed to change password", 400));
+  }
+
+  const filteredBody = filterBody(req.body, 'name', 'email', 'photo');
+
+  const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: { user },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    data: { users },
+  });
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
