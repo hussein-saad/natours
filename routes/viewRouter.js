bcd3c84bbf73929d701696ef27f1663168fe1066
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-20 00:19:22 +0200

    Add login and signup routes to viewRouter.js

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -7,4 +7,7 @@ const router = express.Router();
 router.get('/', viewController.getOverview);
 router.get('/tour/:slug', viewController.getTourDetails);
 
+router.get('/login', viewController.getLoginForm);
+router.get('/signup', viewController.getSingupForm);
+
 module.exports = router;
