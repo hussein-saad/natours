692ea38b2cac51474afda804cc9d002f5822ec2b
Hussein Saad <hussein.saad2021@gmail.com>
2024-02-24 16:10:34 +0200

    Add user data and password update functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData, updatePassword } from './updateSettings';
 
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
@@ -13,3 +16,26 @@ if (loginForm)
   });
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+if (userForm) {
+  userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const currentPassword = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updatePassword(currentPassword, password, passwordConfirm);
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+    document.querySelector('.btn--save-password').textContent = 'save password';
+  });
+}
