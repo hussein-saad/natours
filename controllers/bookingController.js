a32076b3ff8f72e8d172a9070b07574a840c2f7f
Hussein Saad <hussein.saad2021@gmail.com>
2024-03-27 01:46:28 +0200

    Add booking functionality and routes

---
+++ controllers/bookingController.js
@@ -0,0 +1,63 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+const handler = require('./handler');
+
+exports.checkout = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+            ],
+          },
+          unit_amount: tour.price * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) {
+    return next();
+  }
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+
+  next();
+});
+
+exports.getAllBookings = handler.getAll(Booking);
+
+exports.getBooking = handler.getOne(Booking);
+
+exports.createBooking = handler.createOne(Booking);
+
+exports.deleteBooking = handler.deleteOne(Booking);
+
+exports.updateBooking = handler.updateOne(Booking);
